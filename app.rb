c2216b5dee3025d3150a5e6522996c6a85077d5f
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-06 13:26:10 +0100

    Hitpoints minus 10 after attack

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class Battle < Sinatra::Base
   #  enable :sessions
@@ -9,20 +10,22 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:name_player_1]).name
-    $player_2 = Player.new(params[:name_player_2]).name
+    $player_1 = Player.new(params[:name_player_1])
+    $player_2 = Player.new(params[:name_player_2])
     redirect '/play'
   end
 
   get '/play' do
-    @name_player_1 = $player_1
-    @name_player_2 = $player_2
+    @name_player_1 = $player_1.name
+    @name_player_2 = $player_2.name
+    @hit_points = $player_2.hit_points
     erb :play
   end
 
   get '/attack' do
-    @name_player_1 = $player_1
-    @name_player_2 = $player_2
+    @name_player_1 = $player_1.name
+    @name_player_2 = $player_2.name
+    @hit_points = $player_2.hit_points
     erb :attack
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hit_points
   def initialize(name)
     @name = name
+    @hit_points = 60
   end
 end
