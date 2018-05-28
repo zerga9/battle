5fc5813a727bf9d6f4a01eca14d7aa571485e9c3
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-28 19:18:26 +0100

    test added when player 2 loses

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -10,6 +10,17 @@ feature 'Game over' do
     scenario 'Player 1 loses' do
       expect(page).to have_content 'Theo loses!'
     end
+  end
 
+  context 'when Player 2 reaches 0 HP first' do
+    before do
+      sign_in_and_play
+      allow(Kernel).to receive(:rand).and_return(60)
+      click_button 'Attack'
+    end
+
+    scenario 'Player 2 loses' do
+      expect(page).to have_content 'Mary loses!'
+    end
   end
 end
